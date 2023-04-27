ae9c39903bc0645265c3d151557e3bee5895b88b
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-04-27 12:11:43 +0300

    clear log

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const l = (log) => {
-  console.log(log);
-};
 let deferentMonth = 0,
   dateEl = document.getElementById("date"),
   prevEl = document.getElementById("prev"),
@@ -22,61 +19,53 @@ const months = [
 ];
 let weekDays = ["Пн.", "Вт.", "Ср.", "Чт.", "Пт.", "Сб.", "Вс."];
 //функция генерации календаря
-const generate = (count, toDay,start)=>{
-let html = '';
-//цикл для проставления пустых блоков дням, если 1 число начинется с СБ или ВС, или ВТ.
-for(let i = 1; i<start; i++){
-  
-  html += ` <div class="day other"></div>`;
-}
-//цикл создания дней до чсила, сколько их в месяце
-for(let i = 1; i <= count; i++){
-let cl = 'day'
-if(i === toDay){
-  cl += ' today'
-}
-html += ` <div class="${cl}"> ${i} </div> `
-
-}
-daysEl.innerHTML = html;
-}
-//функция вывода даты 
-const printDate = (day, d, year)=>{
-//  months.forEach((item, index) => {
-//    if (index === month) {
-//      date.textContent =( day?day : '' )+ " " + item + " " + year;
-//    }
-//  });
- date.textContent = (day ? day : "")+' ' + months[d.getMonth()] + " " + year;
-}
+const generate = (count, toDay, start) => {
+  let html = "";
+  //цикл для проставления пустых блоков дням, если 1 число начинется с СБ или ВС, или ВТ.
+  for (let i = 1; i < start; i++) {
+    html += ` <div class="day other"></div>`;
+  }
+  //цикл создания дней до чсила, сколько их в месяце
+  for (let i = 1; i <= count; i++) {
+    let cl = "day";
+    if (i === toDay) {
+      cl += " today";
+    }
+    html += ` <div class="${cl}"> ${i} </div> `;
+  }
+  daysEl.innerHTML = html;
+};
+//функция вывода даты
+const printDate = (day, d, year) => {
+  //  months.forEach((item, index) => {
+  //    if (index === month) {
+  //      date.textContent =( day?day : '' )+ " " + item + " " + year;
+  //    }
+  //  });
+  date.textContent = (day ? day : "") + " " + months[d.getMonth()] + " " + year;
+};
 //функция создания календаря
-const create = ()=>{
-let d = new Date();//создает новую дату на сегодняшний день
-d.setMonth(d.getMonth() + deferentMonth);
-let year = d.getFullYear(),
+const create = () => {
+  let d = new Date(); //создает новую дату на сегодняшний день
+  d.setMonth(d.getMonth() + deferentMonth);
+  let year = d.getFullYear(),
     month = d.getMonth(),
-    dayCount = (new Date(year, month + 1, 0)).getDate() //получили кол-во дней в месяце
-    dayStart = (new Date(year,month, 1)).getDay() // получили день недели, на который выпал 1 день месяца
-   
-    toDay = null;
-   
-    
-    if(dayStart === 0){
-      dayStart = 7
-    }
+    dayCount = new Date(year, month + 1, 0).getDate(); //получили кол-во дней в месяце
+  dayStart = new Date(year, month, 1).getDay(); // получили день недели, на который выпал 1 день месяца
 
-    if(deferentMonth === 0) {
-      toDay = d.getDate()
-    }
+  toDay = null;
 
-    printDate(toDay, d, year)
-    generate(dayCount, toDay, dayStart)
-   
-   
+  if (dayStart === 0) {
+    dayStart = 7;
+  }
 
+  if (deferentMonth === 0) {
+    toDay = d.getDate();
+  }
 
-}
-
+  printDate(toDay, d, year);
+  generate(dayCount, toDay, dayStart);
+};
 
 const prev = () => {
   deferentMonth--;
@@ -85,7 +74,7 @@ const prev = () => {
 
 const next = () => {
   deferentMonth++;
-  create()
+  create();
 };
 
 create();
